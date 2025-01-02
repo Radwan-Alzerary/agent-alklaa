c38ee6e24aa54ac262e22d7a7c3df66aa11560a0
Radwan-Alzerary <rdoaniv@ymail.com>
2025-01-02 10:24:12 +0300

    v0.0017

--- backend/app.js
+++ backend/app.js
@@ -33,7 +33,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   // If you need credentials (cookies, etc.) from the frontend:
-  // res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
   next();
 });
 
